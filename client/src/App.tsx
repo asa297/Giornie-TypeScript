8d316942eac33a96a4da4cdbcea64e0135fa26e7
SuperAuMan <makejack4@gmail.com>
2019-09-15 23:24:03 +0700

    init tsconfig for suggestion

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+// import { AuFunction } from '@app/test'
 
 const App: React.SFC = () => {
   return (
@@ -10,17 +11,12 @@ const App: React.SFC = () => {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
