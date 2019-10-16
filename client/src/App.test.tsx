e00ab855672ec3393b75a42cce2eea5827ebfd89
SuperAuMan <makejack4@gmail.com>
2019-10-16 20:32:13 +0700

    add lib test

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import Adapter from 'enzyme-adapter-react-16'
+import { shallow, configure } from 'enzyme'
+import { Provider } from 'react-redux'
+import configureMockStore from 'redux-mock-store'
 
 import App from './App'
 
+configure({ adapter: new Adapter() })
+const mockStore = configureMockStore()
+const store = mockStore({})
+
 it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
-  ReactDOM.unmountComponentAtNode(div)
+  const wrapper = shallow(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+  )
+  expect(wrapper.contains(<App />)).toBe(true)
 })
